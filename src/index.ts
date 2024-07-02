c43146c56b0c0466a8b091035a20ca9abff9e790
Renato Rodrigues <renato@vulppi.dev>
2024-07-02 18:26:53 -0300

    fix: add module jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+/**
+ * @module @vulppi/toolbelt
+ * @description
+ * A collection of tools and utilities for Node.js and TypeScript.
+ *
+ * @author Renato Rodrigues <renato@vulppi.dev>
+ * @license MIT
+ * @version 1.0.1
+ * @keywords node, typescript, toolbelt, vulppi, tools, utilities, help, helpers
+ * @see {@link https://github.com/vulppi-dev/ts-toolbelt}
+ * @see {@link https://www.npmjs.com/package/@vulppi/toolbelt}
+ *
+ * @example promiseDelay function
+ * ```ts
+ * import { promiseDelay } from '@vulppi/toolbelt'
+ *
+ * await promiseDelay(1000)
+ * ```
+ *
+ */
+
 export * from './functions'
 export * from './parser'
 export * from './types'
